3b25244c2619f60b82c2348dbd7f2ef6178760bf
Kalob Reinholz <47434477+Kalob98@users.noreply.github.com>
2022-06-29 05:46:26 -0400

    Delete database.php

--- database/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//connecting to the database
-$dbHost = "localhost";
-$dbUserName = "root";
-$dbPassword = "root";
-$dbName = "nfl";
-
-$conn = mysqli_connect($dbHost, $dbUserName, $dbPassword, $dbName);
